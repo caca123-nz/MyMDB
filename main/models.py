f915b74ac01fba0dd13ff80bb61efd7704104e2d
caca123-nz <nzomopeguy@gmail.com>
2020-11-16 17:20:49 +0100

    Create and add the main application; create the table Movie with fields and first instance (row)

---
+++ main/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+class Movie(models.Model):
+  '''Creating db table for movies'''
+
+  NOT_RATED = 0
+  RATED_G = 1
+  RATED_PG = 2
+  RATED_R = 3
+  RATINGS = (
+        (NOT_RATED, "NR - Not Rated"),
+        (RATED_G, "G - General Audiences"),
+        (RATED_PG, "PG - Parental Guidance Suggested"),
+        (RATED_R, "R - Restricted"))
+  
+  title = models.CharField(max_length=140)
+  plot = models.TextField()
+  year = models.PositiveIntegerField()
+  rating = models.IntegerField(choices=RATINGS, default=NOT_RATED
+  )
+  runtime = models.PositiveIntegerField()
+  website = models.URLField(blank=True)
+
+  class Meta:
+    # Table options
+    db_table = "movie"
+
+  def __str__(self):
+    # String representaion on print
+    return '{} ({})'.format(self.title, self.year)
+
+
+
+
