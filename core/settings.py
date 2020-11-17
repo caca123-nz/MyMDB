9cf60241adba777d6a5206cf37eb195964549d91
caca123-nz <nzomopeguy@gmail.com>
2020-11-17 04:04:12 +0100

    Adding Login and Logout functionality using Custom authenticationform and LoginView as well as LogoutView

--- core/settings.py
+++ core/settings.py
@@ -122,3 +122,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR/ "static"]
 # STATIC_ROOT = "static/"
+
+LOGIN_REDIRECT_URL = "main:movie-list"
--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,12 @@
+from django.contrib.auth import views as auth_views
 from django.urls import re_path
 
+from user.forms import CustomAuthenticationForm
 from user import views
 
 app_name="user"
 urlpatterns = [
-  re_path(r"^register/$", views.RegisterView.as_view(), name="register")
+  re_path(r"^register/$", views.RegisterView.as_view(), name="register"),
+  re_path(r"^login/$", auth_views.LoginView.as_view(authentication_form = CustomAuthenticationForm ), name="login"),
+  re_path(r"^logout/$", auth_views.LogoutView.as_view(), name="logout")
 ]
